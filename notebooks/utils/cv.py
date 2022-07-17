2476949395f00c68776810a916bb18b54dc346c5
king398 <mithilsalunkhe@outlook.com>
2022-07-17 20:48:29 +0530

    new subs

--- notebooks/utils/cv.py
+++ notebooks/utils/cv.py
@@ -3,7 +3,7 @@ import numpy as np
 import statistics
 
 paths = glob.glob(
-    '/home/mithil/PycharmProjects/Rice/models/convnext_base_384_in22ft1k_mixup_rgbonly_512_image_size/*.pth')
+    '/home/mithil/PycharmProjects/Rice/models/fix_csv_models/convnext_small_512_cutmix/*.pth')
 cv = []
 for i in paths:
     i = i.split(".pth")[0]
