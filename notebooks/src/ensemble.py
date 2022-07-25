bf773f7b4d0e50ee6b3ecbddd55952e4923075c9
king398 <mithilsalunkhe@outlook.com>
2022-07-25 15:37:58 +0530

    ConvNext base

--- notebooks/src/ensemble.py
+++ notebooks/src/ensemble.py
@@ -7,9 +7,9 @@ id = \
         'filename'].values
 probablity_1 = np.load('/home/mithil/PycharmProjects/Rice/submission/convnext_small_512_image_size_no_aug_tta.npy',
                        allow_pickle=True)
-probablity_2 = np.load('/home/mithil/PycharmProjects/Rice/submission/convnext_small_384_image_size.npy',
+probablity_2 = np.load('/home/mithil/PycharmProjects/Rice/submission/convnext_small_384_in22ft1k_512_cutmix_tta.npy',
                        allow_pickle=True)
-probablity = probablity_1 * 0.55 + probablity_2 * 0.45
+probablity = probablity_1 * 0.65 + probablity_2 * 0.35
 blast = []
 brown = []
 healthy = []
@@ -19,4 +19,4 @@ for i in probabilitys:
     brown.append(i[1])
     healthy.append(i[2])
 sub = pd.DataFrame({"filename": id, "blast": blast, "brown": brown, "healthy": healthy})
-sub.to_csv('/home/mithil/PycharmProjects/Rice/submission/ensemble/384_512_convnext_ensemble_2.csv', index=False)
+sub.to_csv('/home/mithil/PycharmProjects/Rice/submission/ensemble/cutmix_512_convnext_ensemble.csv', index=False)
